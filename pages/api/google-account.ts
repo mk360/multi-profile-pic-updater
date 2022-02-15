2b25f101ee29f13b15177926c6121c1d774ddcf0
mk360 <hamoudi191@hotmail.fr>
2022-02-16 00:49:50 +0100

    fetch google profile

--- pages/api/google-account.ts
+++ pages/api/google-account.ts
@@ -1,16 +1,10 @@
 import { NextApiResponse } from "next";
-import { google } from 'googleapis';
+import oauth2Client from "../../utils/oauth-client";
 
 async function getGoogleAccount(_: any, res: NextApiResponse<{ url: string }>) {
-    const oauthClient = new google.auth.OAuth2(
-        process.env.GOOGLE_CLIENT_ID!,
-        process.env.GOOGLE_CLIENT_SECRET!,
-        process.env.GOOGLE_REDIRECT_URL!
-    );
-
     const scope = 'https://www.googleapis.com/auth/userinfo.profile';
 
-    const url = oauthClient.generateAuthUrl({ scope });
+    const url = oauth2Client.generateAuthUrl({ scope });
 
     res.status(200).json({ url });
 };
--- pages/api/google-redirect.ts
+++ pages/api/google-redirect.ts
@@ -1,12 +1,26 @@
-import { NextApiResponse } from "next";
+import { NextApiRequest, NextApiResponse } from "next";
+import oauth2Client from "../../utils/oauth-client";
+import { google, people_v1 } from "googleapis";
 
-function A(_: any, res: NextApiResponse) {
-    console.log(res);
-    fetch('https://people.googleapis.com/v1/people/me?personFields=names,photos&key=' + process.env.GOOGLE_API_KEY!).then((r) => {
-        return r.json();
-    }).then(d => {
-        res.status(200).json(d);
+async function GoogleOAuth(req: NextApiRequest, res: NextApiResponse) {
+    const { code } = req.query;
+    const { tokens } = await oauth2Client.getToken(code.toString());
+    oauth2Client.setCredentials(tokens);
+    const { data } = await google.people('v1').people.get({
+        auth: oauth2Client,
+        resourceName: 'people/me',
+        personFields: 'names,photos',
     });
+
+    const names = data.names as people_v1.Schema$Name[];
+    const firstEntry = names[0];
+    const [firstPhoto] = data.photos as people_v1.Schema$Photo[];
+
+    const { displayName, metadata } = firstEntry;
+    const id = metadata?.source?.id;
+    const photoUrl = firstPhoto.url;
+
+    res.redirect(301, '/');
 };
 
-export default A;
+export default GoogleOAuth;
