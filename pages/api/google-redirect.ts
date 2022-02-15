69a6df3cd84a057cb43bd711b4e2731c0bd6535b
mk360 <hamoudi191@hotmail.fr>
2022-02-15 02:08:19 +0100

    get user data from people api

--- pages/api/google-redirect.ts
+++ pages/api/google-redirect.ts
@@ -1,7 +1,12 @@
 import { NextApiResponse } from "next";
 
 function A(_: any, res: NextApiResponse) {
-    console.log({ _ });
+    console.log(res);
+    fetch('https://people.googleapis.com/v1/people/me?personFields=names,photos&key=' + process.env.GOOGLE_API_KEY!).then((r) => {
+        return r.json();
+    }).then(d => {
+        res.status(200).json(d);
+    });
 };
 
 export default A;
